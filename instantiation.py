6daf5ad608822f9d8fb92931f91216e26c41358d
Sebastian Mosbach <sm453@cam.ac.uk>
2024-10-15 16:51:25 +0100

    dev-api: Added custom uniqueness check for Redner instances to XML instantiation.

--- instantiation.py
+++ instantiation.py
@@ -87,6 +87,7 @@ class ABox:
             elif node.tag in self.tbox_customisations[TC_SHORTCUTS_WP]:
                 if self.tbox_customisations[TC_SHORTCUTS_WP][node.tag] == parent.tag:
                     do_shortcut = True
+            is_new_inst = True
             if do_shortcut:
                 log_msg(f"Short-cutting node '{node.tag}'.")
                 # If short-cut, don't add any statements to the graph. Just recurse,
@@ -97,39 +98,64 @@ class ABox:
             else:
                 attribs = node.items()
                 class_name = node.tag.capitalize()
+                class_iri = self.base_iri+class_name
                 if (len(node) > 0) or (len(attribs) > 0):
-                    # This needs to be a new instance, not a literal.
-                    # TODO: We need to check uniqueness of redner using ID prior to instantiation!
-                    inst_iri = generate_instance_iri(self.base_iri, class_name)
-                    new_ref = URIRef(inst_iri)
-                    self.graph.add((new_ref,
-                        RDF.type , URIRef(self.base_iri+class_name)))
-                    log_msg(f"Created instance '{inst_iri}'.")
-                    # Represent node attributes as literals using datatype properties.
-                    for attrib in attribs:
-                        rel = URIRef(f"{self.base_iri}hat{attrib[0].capitalize()}")
-                        self.graph.add((new_ref,
-                            rel , Literal(attrib[1])))
-                    # Add node text as value datatype property.
-                    if node.text is not None:
-                        value = node.text.strip()
-                        if value != "":
-                            rel = URIRef(f"{self.base_iri}hatValue")
-                            self.graph.add((new_ref, rel , Literal(value)))
+                    # This needs to be an instance, not a literal.
+                    if class_name == "Redner":
+                        # We need to check uniqueness prior to instantiation!
+                        rid = ""
+                        for a in attribs:
+                            if a[0] == "id":
+                                rid = a[1]
+                                break
+                        rel_iri = self.base_iri+"hatId"
+                        sb = SPARQLSelectBuilder()
+                        c_var_name = "c"
+                        sb.addVar(makeVarRef(c_var_name))
+                        sb.addWhere(makeVarRef(c_var_name),
+                            makeIRIRef(RDF_TYPE), makeIRIRef(class_iri))
+                        sb.addWhere(makeVarRef(c_var_name),
+                            makeIRIRef(rel_iri), makeLiteralStr(rid, XSD_STRING))
+                        reply = self.store_client.query(sb.build())
+                        if len(reply["results"]["bindings"]) > 0:
+                            # An instance already exists.
+                            inst_iri = reply["results"]["bindings"][0][c_var_name]["value"]
+                            inst_ref = URIRef(inst_iri)
+                            log_msg(f"Re-using instance '{inst_iri}'.")
+                            is_new_inst = False
+                    if is_new_inst:
+                        # No suitable instance exists. Create a new one.
+                        inst_iri = generate_instance_iri(self.base_iri, class_name)
+                        inst_ref = URIRef(inst_iri)
+                        self.graph.add((inst_ref,
+                            RDF.type , URIRef(class_iri)))
+                        log_msg(f"Created instance '{inst_iri}'.")
+                        # Represent node attributes as literals using datatype properties.
+                        for attrib in attribs:
+                            rel = URIRef(f"{self.base_iri}hat{attrib[0].capitalize()}")
+                            self.graph.add((inst_ref,
+                                rel , Literal(attrib[1], datatype=XSD.string)))
+                        # Add node text as value datatype property.
+                        if node.text is not None:
+                            value = node.text.strip()
+                            if value != "":
+                                rel = URIRef(f"{self.base_iri}hatValue")
+                                self.graph.add((inst_ref, rel , Literal(value)))
                 else:
                     # This node has neither children nor attributes. Its text
                     # content will appear as a literal in a datatype property.
-                    new_ref = Literal(node.text)
+                    inst_ref = Literal(node.text)
                 if parent is not None:
-                    # Relate the parent to the new instance/literal.
+                    # Relate the parent to the new/existing instance/literal.
                     rel = URIRef(f"{self.base_iri}hat{class_name}")
-                    self.graph.add((parent_iri_ref, rel, new_ref))
+                    self.graph.add((parent_iri_ref, rel, inst_ref))
                 effective_parent = node
-                effective_parent_iri_ref = new_ref
+                effective_parent_iri_ref = inst_ref
             # Instantiate children, if any, recursively.
-            for child in node:
-                self.instantiate_xml_node(child, parent=effective_parent,
-                    parent_iri_ref=effective_parent_iri_ref)
+            if is_new_inst:
+                for child in node:
+                    self.instantiate_xml_node(child, parent=effective_parent,
+                        parent_iri_ref=effective_parent_iri_ref)
 
 if __name__ == "__main__":
     download_folder = os.path.join("data", "raw")
