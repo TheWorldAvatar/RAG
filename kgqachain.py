90a0b014bec0fb142395c3af8949e68ff121ea95
Sebastian Mosbach <sm453@cam.ac.uk>
2024-11-06 17:27:50 +0000

    dev-rag: Replaced LangChain graph class with store client, as the former does not seem to work with blazegraph.

--- kgqachain.py
+++ kgqachain.py
@@ -18,15 +18,160 @@ from langchain_community.chains.graph_qa.prompts import (
     SPARQL_INTENT_PROMPT,
     SPARQL_QA_PROMPT,
 )
-from langchain_community.graphs.rdf_graph import RdfGraph
 
+from rdflib.query import ResultRow
+from rdflib import Variable, URIRef, Literal
+import storeclient
+from CommonNamespaces import nameFromIRI
+
+prefixes = {
+    "owl": """PREFIX owl: <http://www.w3.org/2002/07/owl#>\n""",
+    "rdf": """PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n""",
+    "rdfs": """PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n""",
+    "xsd": """PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n""",
+}
+
+cls_query_rdf = prefixes["rdfs"] + (
+    """SELECT DISTINCT ?cls ?com\n"""
+    """WHERE { \n"""
+    """    ?instance a ?cls . \n"""
+    """    OPTIONAL { ?cls rdfs:comment ?com } \n"""
+    """}"""
+)
+
+cls_query_rdfs = prefixes["rdfs"] + (
+    """SELECT DISTINCT ?cls ?com\n"""
+    """WHERE { \n"""
+    """    ?instance a/rdfs:subClassOf* ?cls . \n"""
+    """    OPTIONAL { ?cls rdfs:comment ?com } \n"""
+    """}"""
+)
+
+cls_query_owl = prefixes["rdfs"] + (
+    """SELECT DISTINCT ?cls ?com\n"""
+    """WHERE { \n"""
+    """    ?instance a/rdfs:subClassOf* ?cls . \n"""
+    """    FILTER (isIRI(?cls)) . \n"""
+    """    OPTIONAL { ?cls rdfs:comment ?com } \n"""
+    """}"""
+)
+
+rel_query_rdf = prefixes["rdfs"] + (
+    """SELECT DISTINCT ?rel ?com\n"""
+    """WHERE { \n"""
+    """    ?subj ?rel ?obj . \n"""
+    """    OPTIONAL { ?rel rdfs:comment ?com } \n"""
+    """}"""
+)
+
+rel_query_rdfs = (
+    prefixes["rdf"]
+    + prefixes["rdfs"]
+    + (
+        """SELECT DISTINCT ?rel ?com\n"""
+        """WHERE { \n"""
+        """    ?rel a/rdfs:subPropertyOf* rdf:Property . \n"""
+        """    OPTIONAL { ?rel rdfs:comment ?com } \n"""
+        """}"""
+    )
+)
+
+op_query_owl = (
+    prefixes["rdfs"]
+    + prefixes["owl"]
+    + (
+        """SELECT DISTINCT ?op ?com\n"""
+        """WHERE { \n"""
+        """    ?op a/rdfs:subPropertyOf* owl:ObjectProperty . \n"""
+        """    OPTIONAL { ?op rdfs:comment ?com } \n"""
+        """}"""
+    )
+)
+
+dp_query_owl = (
+    prefixes["rdfs"]
+    + prefixes["owl"]
+    + (
+        """SELECT DISTINCT ?dp ?com\n"""
+        """WHERE { \n"""
+        """    ?dp a/rdfs:subPropertyOf* owl:DatatypeProperty . \n"""
+        """    OPTIONAL { ?dp rdfs:comment ?com } \n"""
+        """}"""
+    )
+)
+
+def _res_to_str(res: dict, var: str) -> str:
+    iri = res[var]["value"]
+    comment = res["com"]["value"] if "com" in res else ""
+    return (
+        "<"
+        + iri
+        + "> ("
+        + nameFromIRI(iri)
+        + ", "
+        + comment
+        + ")"
+    )
+
+def get_store_schema(sc: storeclient.StoreClient, standard: str) -> str:
+    if standard == "custom":
+        clss = sc.query(cls_query_rdfs)["results"]["bindings"]
+        rels = sc.query(rel_query_rdf)["results"]["bindings"]
+        return (
+            f"In the following, each IRI is followed by the local name and "
+            f"optionally its description in parentheses. \n"
+            f"The RDF graph supports the following node types:\n"
+            f'{", ".join([_res_to_str(r, "cls") for r in clss])}\n'
+            f"The RDF graph supports the following relationships:\n"
+            f'{", ".join([_res_to_str(r, "rel") for r in rels])}\n'
+        )
+    elif standard == "rdfs":
+        clss = sc.query(cls_query_rdfs)["results"]["bindings"]
+        rels = sc.query(rel_query_rdfs)["results"]["bindings"]
+        return (
+            f"In the following, each IRI is followed by the local name and "
+            f"optionally its description in parentheses. \n"
+            f"The RDF graph supports the following node types:\n"
+            f'{", ".join([_res_to_str(r, "cls") for r in clss])}\n'
+            f"The RDF graph supports the following relationships:\n"
+            f'{", ".join([_res_to_str(r, "rel") for r in rels])}\n'
+        )
+    elif standard == "owl":
+        clss = sc.query(cls_query_owl)["results"]["bindings"]
+        ops = sc.query(op_query_owl)["results"]["bindings"]
+        dps = sc.query(dp_query_owl)["results"]["bindings"]
+        return (
+            f"In the following, each IRI is followed by the local name and "
+            f"optionally its description in parentheses. \n"
+            f"The OWL graph supports the following node types:\n"
+            f'{", ".join([_res_to_str(r, "cls") for r in clss])}\n'
+            f"The OWL graph supports the following object properties, "
+            f"i.e., relationships between objects:\n"
+            f'{", ".join([_res_to_str(r, "op") for r in ops])}\n'
+            f"The OWL graph supports the following data properties, "
+            f"i.e., relationships between objects and literals:\n"
+            f'{", ".join([_res_to_str(r, "dp") for r in dps])}\n'
+        )
+    else:
+        raise ValueError(f"Schema mode '{standard}' is not supported!")
+
+def _make_result_row(r: dict) -> ResultRow:
+    values = {}
+    labels = []
+    for varname in r:
+        value = r[varname]["value"]
+        values[Variable(varname)] = (
+            URIRef(value) if value.startswith("http") else Literal(value)
+        )
+        labels.append(Variable(varname))
+    return ResultRow(values, labels)
 
 class GraphSparqlQAChain(Chain):
     """
     Question-answering against an RDF or OWL graph by generating SPARQL statements.
     """
 
-    graph: RdfGraph = Field(exclude=True)
+    store_client: storeclient.StoreClient = Field(exclude=True)
     sparql_generation_select_chain: LLMChain
     sparql_intent_chain: LLMChain
     qa_chain: LLMChain
@@ -102,7 +247,9 @@ class GraphSparqlQAChain(Chain):
         _run_manager.on_text(intent, color="green", end="\n", verbose=self.verbose)
 
         generated_sparql = sparql_generation_chain.run(
-            {"prompt": prompt, "schema": self.graph.get_schema}, callbacks=callbacks
+            {"prompt": prompt,
+             "schema": get_store_schema(self.store_client, "custom")},
+            callbacks=callbacks
         )
 
         _run_manager.on_text("Generated SPARQL:", end="\n", verbose=self.verbose)
@@ -111,7 +258,9 @@ class GraphSparqlQAChain(Chain):
         )
 
         if intent == "SELECT":
-            context = self.graph.query(generated_sparql)
+            reply = self.store_client.query(generated_sparql)["results"]["bindings"]
+            # Turn reply dictionary into list of result rows.
+            context = [_make_result_row(r) for r in reply]
 
             _run_manager.on_text("Full Context:", end="\n", verbose=self.verbose)
             _run_manager.on_text(
