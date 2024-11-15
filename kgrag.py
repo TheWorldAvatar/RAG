f21d788d47c33ee8c0b85053898320f616c282d8
Sebastian Mosbach <sm453@cam.ac.uk>
2024-11-15 12:25:40 +0000

    dev-rag: Refactored RAG configuration into its own module.

--- kgrag.py
+++ kgrag.py
@@ -1,45 +1,23 @@
-import yaml
 import os
 from langchain_openai import ChatOpenAI
 
+from ragconfig import *
 import storeclient
 from kgqachain import KGQAChain
 
-# Configuration variable names
-CVN_ENDPOINT       = "Endpoint"
-CVN_MODEL          = "Model"
-CVN_OPENAI_API_KEY = "OPENAI_API_KEY"
-CVN_TEMPERATURE    = "Temperature"
-CONFIG_VAR_NAMES = [CVN_ENDPOINT, CVN_MODEL, CVN_OPENAI_API_KEY, CVN_TEMPERATURE]
-
-# Read configuration file and check contents
-with open("config.yaml", "r") as file_object:
-    config = yaml.safe_load(file_object)
-for v in CONFIG_VAR_NAMES:
-    if v in config:
-        if config[v] is None:
-            raise ValueError(f"The '{v}' provided "
-                f"in the configuration file is empty!")
-    else:
-        raise NameError(f"No '{v}' provided in configuration file!")
-
-# Set OpenAI API key
-if config[CVN_OPENAI_API_KEY] != "":
-    if os.environ.get(CVN_OPENAI_API_KEY):
-        print(f"WARNING: Environment variable '{CVN_OPENAI_API_KEY}' is set.")
-        print("It will be overridden by the value provided in the configuration file!")
-    os.environ[CVN_OPENAI_API_KEY] = config[CVN_OPENAI_API_KEY]
-else:
-    if not os.environ.get(CVN_OPENAI_API_KEY):
-        raise NameError(f"No '{CVN_OPENAI_API_KEY}' provided in either"
-            f"configuration file or environment variables!")
+config = RAGConfig("config.yaml")
+config.check()
+config.set_openai_api_key()
 
 with open(os.path.join("data", "processed", "MDB_STAMMDATEN-xml-tbox-description.txt"), "r") as f:
     schema = f.read()
 
-sc = storeclient.RemoteStoreClient(config[CVN_ENDPOINT])
+sc = storeclient.RemoteStoreClient(config.get(CVN_ENDPOINT))
 
-llm = ChatOpenAI(temperature=config[CVN_TEMPERATURE], model=config[CVN_MODEL])
+llm = ChatOpenAI(
+    model=config.get(CVN_MODEL),
+    temperature=config.get(CVN_TEMPERATURE)
+)
 
 chain = KGQAChain.from_llm(
     llm, store_client=sc, schema_description=schema,
---
+++ ragconfig.py
@@ -0,0 +1,52 @@
+import yaml
+import os
+
+# Configuration variable names
+CVN_ENDPOINT       = "Endpoint"
+CVN_MODEL          = "Model"
+CVN_OPENAI_API_KEY = "OPENAI_API_KEY"
+CVN_TEMPERATURE    = "Temperature"
+CONFIG_VAR_NAMES = [CVN_ENDPOINT, CVN_MODEL, CVN_OPENAI_API_KEY, CVN_TEMPERATURE]
+
+class RAGConfig:
+
+    def __init__(self, yaml_file_name: str) -> None:
+        """
+        Reads a YAML configuration file.
+        """
+        with open(yaml_file_name, "r") as file_object:
+            self._config = yaml.safe_load(file_object)
+
+    def get(self, var_name: str):
+        """
+        Returns the value of the variable with the given name.
+        """
+        if var_name in self._config:
+            return self._config[var_name]
+        else:
+            raise NameError(f"No '{var_name}' provided in configuration!")
+
+    def check(self) -> None:
+        """
+        Checks for presence and non-emptiness of all expected
+        configuration variables.
+        """
+        for v in CONFIG_VAR_NAMES:
+            if self.get(v) is None:
+                raise ValueError(f"The '{v}' provided "
+                    f"in the configuration file is empty!")
+
+    def set_openai_api_key(self) -> None:
+        """
+        Sets the configured OpenAI API key as an environment variable.
+        """
+        new_key = self.get(CVN_OPENAI_API_KEY)
+        if new_key != "":
+            if os.environ.get(CVN_OPENAI_API_KEY):
+                print(f"WARNING: Environment variable '{CVN_OPENAI_API_KEY}' is set.")
+                print("It will be overridden by the value provided in the configuration file!")
+            os.environ[CVN_OPENAI_API_KEY] = new_key
+        else:
+            if not os.environ.get(CVN_OPENAI_API_KEY):
+                raise NameError(f"No '{CVN_OPENAI_API_KEY}' provided in either"
+                    f"configuration file or environment variables!")
