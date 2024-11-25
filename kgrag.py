3d92ccc95f799d38072d5558d3f8e7fcc888370e
Sebastian Mosbach <sm453@cam.ac.uk>
2024-11-25 14:46:16 +0000

    dev-rag: Refactored KG-RAG into a structure similar to base RAG.

--- kgrag.py
+++ kgrag.py
@@ -1,37 +1,56 @@
 import os
 from langchain_openai import ChatOpenAI
 
+from common import *
 from ragconfig import *
 from storeclient import RemoteStoreClient
 from kgqachain import KGQAChain, get_store_schema
-from common import *
 
-config = RAGConfig("config.yaml")
-config.check()
-config.set_openai_api_key()
+class KGRAG:
 
-sc = RemoteStoreClient(config.get(CVN_ENDPOINT))
+    def __init__(self, config: RAGConfig) -> None:
+        sc = RemoteStoreClient(config.get(CVN_ENDPOINT))
+        #with open(os.path.join("data", "processed",
+        #    "MDB_STAMMDATEN-xml-tbox-description.txt"), "r") as f:
+        #    schema = f.read()
+        schema = get_store_schema(
+            RemoteStoreClient(config.get(CVN_TBOX_ENDPOINT)),
+            {MMD_PREFIX: MMD_BASE_IRI}
+        )
+        llm = ChatOpenAI(
+            model=config.get(CVN_MODEL),
+            temperature=config.get(CVN_TEMPERATURE)
+        )
+        self.chain = KGQAChain.from_llm(
+            llm, store_client=sc, schema_description=schema,
+            verbose=True, return_sparql_query=True
+        )
 
-#with open(os.path.join("data", "processed", "MDB_STAMMDATEN-xml-tbox-description.txt"), "r") as f:
-#    schema = f.read()
-schema = get_store_schema(
-    RemoteStoreClient(config.get(CVN_TBOX_ENDPOINT)),
-    {MMD_PREFIX: MMD_BASE_IRI}
-)
+    def query(self, question: str) -> str:
+        """
+        Returns an answer as a string to a given natural
+        language question.
+        WARNING: This may cost real money and may be expensive!
+        """
+        try:
+            response = self.chain.invoke(question)
+            return response[self.chain.output_key]
+        except Exception as e:
+            return f"Error processing query: {str(e)}"
 
-llm = ChatOpenAI(
-    model=config.get(CVN_MODEL),
-    temperature=config.get(CVN_TEMPERATURE)
-)
+def main():
+    logging.basicConfig(filename="kgrag.log", level=logging.INFO)
+    config = RAGConfig("config.yaml")
+    config.check()
+    config.set_openai_api_key()
+    rag = KGRAG(config)
 
-chain = KGQAChain.from_llm(
-    llm, store_client=sc, schema_description=schema,
-    verbose=True, return_sparql_query=True
-)
+    nlq = "Welche ID hat das MdB Adenauer?"
+    #nlq = "Welchen Nachnamen hat das MdB mit ID 11000009?"
+    #nlq = "Wann wurde Willy Brandt geboren?"
+    log_msg(f"Frage: {nlq}")
+    answer = rag.query(nlq)
+    log_msg(f"Antwort: {answer}")
 
-nlq = "Welche ID hat das MdB Adenauer?"
-#nlq = "Welchen Nachnamen hat das MdB mit ID 11000009?"
-#nlq = "Wann wurde Willy Brandt geboren?"
-result = chain.invoke(nlq)
-print(f"Frage: {result[chain.input_key]}")
-print(f"Antwort: {result[chain.output_key]}")
+if __name__ == "__main__":
+    main()
