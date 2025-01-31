59769fae68666bd864ed0050a2b738d1cdaa59b0
Sebastian Mosbach <sm453@cam.ac.uk>
2025-01-31 13:34:27 +0000

    dev-rag: Added first attempt at a hybrid embedding/KG-RAG system.

---
+++ hybridqachain.py
@@ -0,0 +1,209 @@
+from __future__ import annotations
+
+from typing import Any, Dict, List, Optional
+
+from langchain.chains.base import Chain
+from langchain_core.callbacks import CallbackManagerForChainRun
+from langchain_core.language_models import BaseLanguageModel
+from langchain_core.prompts.prompt import PromptTemplate
+from langchain_core.prompt_values import StringPromptValue
+from langchain_core.runnables.base import RunnableSequence
+from langchain.schema.runnable import Runnable
+from langchain_core.documents import Document
+from langchain_core.vectorstores import VectorStoreRetriever
+from pydantic import Field
+
+from common import *
+from ragconfig import *
+from storeclient import StoreClient
+
+class RunnableExtractSource(Runnable):
+    """
+    Extracts the source from the metadata of a list of documents.
+    """
+
+    def invoke(
+        self, input_data: List[Document], config: Dict[str, Any] = None
+    ) -> List[str]:
+        return [d.metadata["source"] for d in input_data]
+
+class RunnableLogInputs(Runnable):
+
+    def invoke(
+        self, input_data: StringPromptValue, config: Dict[str, Any] = None
+    ) -> StringPromptValue:
+        log_msg(input_data.to_string())
+        return input_data
+
+def query_result_pretty_str(result: list[dict[str, str]]) -> str:
+    str_list = ["["]
+    for r in result:
+        str_list.append("(")
+        for varname in r:
+            value = r[varname]["value"]
+            if not value.startswith("http"):
+                str_list.append(f"{varname}: {value},")
+        str_list.append("),")
+    str_list.append("]")
+    return "".join(str_list)
+
+class HybridQAChain(Chain):
+    store_client: StoreClient = Field(exclude=True)
+    schema_description: str
+    threshold_retriever: VectorStoreRetriever = Field(exclude=True)
+    top_k_retriever: VectorStoreRetriever = Field(exclude=True)
+    sparql_gen_or_retrieve_chain: RunnableSequence
+    need_content_chain: RunnableSequence
+    sparql_gen_with_ids_chain: RunnableSequence
+    sparql_gen_with_docs_chain: RunnableSequence
+    answer_gen_chain: RunnableSequence
+    return_sparql_query: bool = False
+    input_key: str = "query"  #: :meta private:
+    output_key: str = "result"  #: :meta private:
+    sparql_query_key: str = "sparql_query"  #: :meta private:
+
+    @property
+    def input_keys(self) -> List[str]:
+        """Return the input keys.
+
+        :meta private:
+        """
+        return [self.input_key]
+
+    @property
+    def output_keys(self) -> List[str]:
+        """Return the output keys.
+
+        :meta private:
+        """
+        _output_keys = [self.output_key]
+        return _output_keys
+
+    @classmethod
+    def from_llm(
+        cls,
+        llm: BaseLanguageModel,
+        sparql_gen_prompt: PromptTemplate,
+        need_content_prompt: PromptTemplate,
+        sparql_gen_with_ids_prompt: PromptTemplate,
+        sparql_gen_with_docs_prompt: PromptTemplate,
+        answer_gen_prompt: PromptTemplate,
+        *,
+        schema_description: str,
+        **kwargs: Any,
+    ) -> HybridQAChain:
+        """Initialise from LLM."""
+        sparql_gen_or_retrieve_chain = (
+            sparql_gen_prompt
+            | RunnableLogInputs()
+            | llm
+        )
+        need_content_chain = (
+            need_content_prompt
+            | RunnableLogInputs()
+            | llm
+        )
+        sparql_gen_with_ids_chain = (
+            sparql_gen_with_ids_prompt
+            | RunnableLogInputs()
+            | llm
+        )
+        sparql_gen_with_docs_chain = (
+            sparql_gen_with_docs_prompt
+            | RunnableLogInputs()
+            | llm
+        )
+        answer_gen_chain = (
+            answer_gen_prompt
+            | RunnableLogInputs()
+            | llm
+        )
+
+        return cls(
+            need_content_chain=need_content_chain,
+            sparql_gen_or_retrieve_chain=sparql_gen_or_retrieve_chain,
+            sparql_gen_with_ids_chain=sparql_gen_with_ids_chain,
+            sparql_gen_with_docs_chain=sparql_gen_with_docs_chain,
+            answer_gen_chain=answer_gen_chain,
+            schema_description=schema_description,
+            **kwargs,
+        )
+
+    def _call(
+        self,
+        inputs: Dict[str, Any],
+        run_manager: Optional[CallbackManagerForChainRun] = None,
+    ) -> Dict[str, str]:
+        """
+        Break the question down into retrieval steps, either from the vector
+        store or from the knowledge graph or both as required, and then use
+        the retrieved information to generate an answer to the question.
+        """
+        #_run_manager = run_manager or CallbackManagerForChainRun.get_noop_manager()
+        #callbacks = _run_manager.get_child()
+        question = inputs[self.input_key]
+
+        # Ask the LLM to generate a SPARQL query. If the query involves
+        # filtering the textual content of speeches, then this means we need
+        # to retrieve the latter from the vector store first. The LLM should
+        # tell us that.
+        schema_and_question_inputs = {
+            "schema": self.schema_description,
+            "question": question
+        }
+        gen_res_str: str = self.sparql_gen_or_retrieve_chain.invoke(
+            schema_and_question_inputs).content
+        query_or_retrieve = gen_res_str.strip(" .`")
+        if query_or_retrieve.lower().startswith("retrieve"):
+            # The LLM told us to retrieve documents from the vector store first.
+            topic = query_or_retrieve[len("retrieve"):].strip(' ."')
+            log_msg(f"Topic to be retrieved from vector store: '{topic}'")
+            need_content_str: str = self.need_content_chain.invoke(
+                schema_and_question_inputs).content
+            ncs = need_content_str.lower().strip(' ."')
+            log_msg(f"Is speech content to be retrieved: '{need_content_str}'")
+            need_content = ("yes" in ncs) or ("ja" in ncs)
+            if need_content:
+                retriever = self.top_k_retriever
+            else:
+                retriever = self.threshold_retriever | RunnableExtractSource()
+            retrieved_from_vs = retriever.invoke(topic)
+            if need_content:
+                # If the content of the speeches is needed, then we don't need
+                # to query the KG at all.
+                sparql_query = ""
+            else:
+                # Otherwise, try again to generate a query, this time with
+                # retrieved information as context.
+                gen_wc_inputs = {
+                    "schema": self.schema_description,
+                    "context": retrieved_from_vs,
+                    "question": question
+                }
+                gen_wc_res_str: str = (
+                    #self.sparql_gen_with_docs_chain.invoke(gen_wc_inputs).content
+                    #if need_content else
+                    self.sparql_gen_with_ids_chain.invoke(gen_wc_inputs).content
+                )
+                sparql_query = gen_wc_res_str
+        else:
+            retrieved_from_vs: list[Document] = []
+            sparql_query = query_or_retrieve
+        # Execute SPARQL query, if necessary
+        if sparql_query.lower().startswith("sparql"):
+            sparql_query = sparql_query[len("sparql"):]
+        log_msg(f"SPARQL query:\n{sparql_query}")
+        if sparql_query != "":
+            reply = self.store_client.query(sparql_query)["results"]["bindings"]
+            retrieved_from_kg = query_result_pretty_str(reply)
+        else:
+            retrieved_from_kg = ""
+        log_msg(f"Retrieved from KG:\n{retrieved_from_kg}")
+        # Generate answer, based on retrieved info
+        answer: str = self.answer_gen_chain.invoke({
+            "context": retrieved_from_kg,
+            "speeches": retrieved_from_vs,
+            "question": question
+        }).content
+
+        return {self.output_key: answer}
---
+++ hybridrag.py
@@ -0,0 +1,187 @@
+from datetime import datetime
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain.embeddings import CacheBackedEmbeddings
+from langchain.storage import LocalFileStore
+from langchain.prompts import PromptTemplate
+from langchain_qdrant import QdrantVectorStore
+from qdrant_client import QdrantClient
+from qdrant_client.http.models import Distance, VectorParams
+
+from common import *
+from ragconfig import *
+from hybridqachain import HybridQAChain
+from storeclient import RemoteStoreClient
+from debateloader import SpeechKGLoader
+from questions import Questions, Answer
+
+class HybridRAG:
+
+    def __init__(self, config: RAGConfig) -> None:
+        store_client = RemoteStoreClient(config.get(CVN_ENDPOINT))
+        schema = get_store_schema(
+            RemoteStoreClient(config.get(CVN_TBOX_ENDPOINT)),
+            {MMD_PREFIX: MMD_BASE_IRI, PD_PREFIX: PD_BASE_IRI}
+        )
+        #schema = read_text_from_file(
+        #    os.path.join("data", "processed", "20137-xml-tbox-description.txt"))
+        #log_msg(schema)
+
+        llm = ChatOpenAI(
+            model=config.get(CVN_MODEL),
+            temperature=config.get(CVN_TEMPERATURE)
+        )
+        sparql_gen_or_retrieve_prompt = PromptTemplate(
+            template=read_text_from_file(
+                os.path.join("prompt_templates", "hybrid_sparql_gen_or_ask_retrieve.txt")
+            ),
+            input_variables=["schema", "question"]
+        )
+        need_content_prompt = PromptTemplate(
+            template=read_text_from_file(
+                os.path.join("prompt_templates", "hybrid_need_content.txt")
+            ),
+            input_variables=["question"]
+        )
+        sparql_gen_with_ids_prompt = PromptTemplate(
+            template=read_text_from_file(
+                os.path.join("prompt_templates", "hybrid_sparql_gen_with_ids.txt")
+            ),
+            input_variables=["schema", "context", "question"]
+        )
+        sparql_gen_with_docs_prompt = PromptTemplate(
+            template=read_text_from_file(
+                os.path.join("prompt_templates", "hybrid_sparql_gen_with_docs.txt")
+            ),
+            input_variables=["schema", "context", "question"]
+        )
+        answer_gen_prompt = PromptTemplate(
+            template=read_text_from_file(
+                os.path.join("prompt_templates", "hybrid_answer_gen.txt")
+            ),
+            input_variables=["context", "speeches", "prompt"]
+        )
+
+        self._init_vector_store(config)
+        # Similarity score threshold retrieval:
+        # NB Maximum number k of retrieved documents is still used!
+        threshold_retriever = self.vector_store.as_retriever(
+            search_type="similarity_score_threshold",
+            search_kwargs={
+                "score_threshold": 0.7,
+                "k": 1000
+            }
+        )
+        # Retriever for the "top k" most similar documents.
+        top_k_retriever = self.vector_store.as_retriever(
+            search_type="similarity", # This is the default search type.
+            search_kwargs={
+                "k": config.get(CVN_TOP_K) # Defaults to 4, apparently, if not given.
+            }
+        )
+        self.chain = HybridQAChain.from_llm(
+            llm, sparql_gen_or_retrieve_prompt, need_content_prompt,
+            sparql_gen_with_ids_prompt, sparql_gen_with_docs_prompt,
+            answer_gen_prompt,
+            threshold_retriever=threshold_retriever,
+            top_k_retriever=top_k_retriever,
+            store_client=store_client, schema_description=schema,
+            verbose=True, return_sparql_query=True
+        )
+
+    def _init_vector_store(self, config: RAGConfig) -> None:
+        # https://platform.openai.com/docs/guides/embeddings/
+        underlying_embeddings = OpenAIEmbeddings(
+            model=config.get(CVN_EMBEDDING_MODEL)
+        )
+        emb_cache_store = LocalFileStore(config.get(CVN_EMBEDDING_CACHE))
+        embeddings = CacheBackedEmbeddings.from_bytes_store(
+            underlying_embeddings, emb_cache_store,
+            namespace=underlying_embeddings.model
+        )
+        collection_name = config.get(CVN_VS_COLLECTION)
+        vs_cache_path = config.get(CVN_VSTORE_CACHE)
+        # If the vector store cache directory exists, we attempt to
+        # read an existing collection.
+        if os.path.isdir(vs_cache_path):
+            log_msg(f"Reading collection '{collection_name}' from "
+                f"existing vector store in '{vs_cache_path}'...")
+            self.vector_store = QdrantVectorStore.from_existing_collection(
+                path=vs_cache_path,
+                collection_name=collection_name,
+                embedding=embeddings
+            )
+        else:
+            log_msg(f"Creating new vector store in '{vs_cache_path}', "
+                f"with new collection '{collection_name}'...")
+            client = QdrantClient(path=vs_cache_path)
+            # NB Unfortunately, at time of writing, there does not
+            # seem to be a good way to determine the dimension of
+            # the embedding, so we read that from config, too.
+            client.create_collection(
+                collection_name=collection_name,
+                vectors_config=VectorParams(
+                    size=config.get(CVN_EMBEDDING_DIM),
+                    distance=Distance.COSINE
+                )
+            )
+            self.vector_store = QdrantVectorStore(
+                client=client,
+                collection_name=collection_name,
+                embedding=embeddings
+            )
+
+    def load_speeches_from_kg(self) -> None:
+        """
+        Queries speeches from the KG and loads them into the vector store.
+        WARNING: This will potentially calculate embeddings for all speeches,
+        if they are not cached already, so this may cost real money
+        and may be expensive!
+        """
+        documents = SpeechKGLoader(self.store_client).load()
+        log_msg(f"Adding {len(documents)} speeches queried from "
+            "the store client to the vector store...")
+        return self.vector_store.add_documents(documents)
+
+    def query(self, question: str) -> str:
+        """
+        Returns an answer as a string to a given natural
+        language question.
+        WARNING: This may cost real money and may be expensive!
+        """
+        try:
+            response = self.chain.invoke(question)
+            return response[self.chain.output_key]
+        except Exception as e:
+            return f"Error processing query: {str(e)}"
+
+def main():
+    logging.basicConfig(filename="hybridrag.log", encoding=ES_UTF_8,
+        level=logging.INFO)
+    config = RAGConfig("config-hybrid.yaml")
+    config.check()
+    config.set_openai_api_key()
+    rag = HybridRAG(config)
+
+    # Load documents, i.e. embed and store in vector store.
+    # WARNING: 1) This may be expensive! 2) Do not load documents
+    # that have already been loaded. This creates duplicates!
+    #rag.load_speeches_from_kg()
+
+    q_catalogue_name = "questions-mine" #mine #A
+    q_cat_save_filename = os.path.join("data",
+        "".join([q_catalogue_name, "-with-answers", ".json"]))
+    q_cat_load_filename = (q_cat_save_filename if
+        os.path.isfile(q_cat_save_filename) else
+        os.path.join("data", "".join([q_catalogue_name, ".json"])))
+    questions = Questions()
+    questions.load(q_cat_load_filename)
+    question = questions.find_question_by_id("1")
+    nlq = question.get_text()
+    log_msg(f"Frage: {nlq}")
+    answer = rag.query(nlq)
+    log_msg(f"Antwort: {answer}")
+    question.add_answer(Answer(answer, "Hybrid-RAG", datetime.now()))
+    #questions.save(q_cat_save_filename)
+
+if __name__ == "__main__":
+    main()
